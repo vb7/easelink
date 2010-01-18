1fd1ecf6b7731643967cf70dd5327c375039b3d0
ashi009 <ashi009@fa61e688-fffe-11de-87c3-653bb6514e15>
2010-01-18 16:47:03 +0000

    + Added "Convert To A Link" feature * Fixed RayFile minor bug (may be that's not a bug, just a network failure) * Fixed some confused label

--- trunk/easelink-1.0-firefox/chrome/content/easelink.js
+++ trunk/easelink-1.0-firefox/chrome/content/easelink.js
@@ -10,7 +10,7 @@ const base64Pattrn = /^([a-zA-Z][0-9a-zA-Z+\-\.]*\:)\/{2}([0-9a-zA-Z\+=\/]+)([0-
 
 var emptyFunction = function() {};
 
-function defaultFix(href) {
+function defaultFixer(href) {
   return function(node) {
     node.setAttribute('href', node.getAttribute(href));
     node.removeAttribute(href);
@@ -19,7 +19,7 @@ function defaultFix(href) {
   };
 }
 
-function defaultDecode(prefix, suffix) {
+function defaultDecoder(prefix, suffix) {
   return function(node) {
     var url = node.getAttribute('href');
     var match;
@@ -34,14 +34,14 @@ const Protocols = {
   'thunder:': {
     prefkey: 'thunder',
     xpath: '[@thunderhref]',
-    fix: defaultFix('thunderhref'),
-    decode: defaultDecode(2, 2)
+    fix: defaultFixer('thunderhref'),
+    decode: defaultDecoder(2, 2)
   },
   'qqdl:': {
     prefkey: 'qqxuanfeng',
     xpath: '[@qhref]',
-    fix: defaultFix('qhref'),
-    decode: defaultDecode(0, 0)
+    fix: defaultFixer('qhref'),
+    decode: defaultDecoder(0, 0)
   },
   'fs2you:': {
     xpath: "[@href and starts-with(translate(@href, 'FSYOU', 'fsyou'), 'fs2you:')]",
@@ -51,7 +51,7 @@ const Protocols = {
       var match;
       if (matches = base64Pattrn.exec(url)) {
         url = atob(matches[2]).toString();
-        node.setAttribute('href', 'http://' + url);
+        node.setAttribute('href', 'http://' + url.substring(0, url.lastIndexOf('|')));
       }
     }
   },
@@ -70,7 +70,7 @@ const Protocols = {
       node.removeAttribute('oncontextmenu');
       node.removeAttribute('onclick');
     },
-    decode: defaultDecode(10, 10)
+    decode: defaultDecoder(10, 10)
   },
   'tencent:': {
     xpath: "[@href and starts-with(translate(@href, 'HTPWAQCOM', 'htpwaqcom'), 'http://wpa.qq.com/')]",
@@ -94,7 +94,7 @@ const Protocols = {
     },
     decode: emptyFunction
   },
-  'namipan:': {
+  '#namipan:': {
     xpath: "[@href and starts-with(translate(@href, 'JAVSCRIPT', 'javscript'), 'javascript:') and contains(translate(@href, 'NAMIPCO', 'namipco'), '.namipan.com')]",
     fix: emptyFunction,
     decode: function(node) {
@@ -143,17 +143,32 @@ const SettingMenu = {
 };
 
 const ContextMenu = {
-  _menu: null,
+  _menudecode: null,
+  _menuconvt: null,
   _decode: null,
+  _selection : null,
+  _selurl : "",
   init: function() {
-    this._menu = document.getElementById('easelink-ctxmu-convt');
-    this._menu.parentNode.addEventListener('popupshowing', this, true);
-    this._menu.addEventListener('command', this, true);
+    this._menudecode = document.getElementById('easelink-ctxmu-decode');
+    this._menuconvt = document.getElementById('easelink-ctxmu-convert');
+    this._menudecode.parentNode.addEventListener('popupshowing', this, true);
+    this._menudecode.addEventListener('command', this, true);
+    this._menuconvt.addEventListener('command', this, true);
+  },
+  _convert: function() {
+    var range = this._selection.getRangeAt(0);
+    var frag = range.extractContents();
+    var link = frag.ownerDocument.createElement('a');
+    link.setAttribute('href', this._selurl);
+    if (prefBranch.getBoolPref('plain')) this._decode(link);
+    link.appendChild(frag);
+    range.insertNode(link);
   },
   handleEvent: function(evt) {
     switch (evt.type) {
     case 'popupshowing':
-      this._menu.hidden = true;
+      this._menudecode.hidden = true;
+      this._menuconvt.hidden = true;
       if (gContextMenu.onLink) {
         var link = gContextMenu.link;
         var doc = link.ownerDocument;
@@ -174,17 +189,30 @@ const ContextMenu = {
           if (plain)
             this._decode(link);
           else
-            this._menu.hidden = this._decode == emptyFunction;
+            this._menudecode.hidden = this._decode == emptyFunction;
         }
         if (fixed || found && plain) {
           gContextMenu.linkURL = gContextMenu.getLinkURL();
           gContextMenu.linkURI = gContextMenu.getLinkURI();
           gContextMenu.linkProtocol = gContextMenu.getLinkProtocol();
         }
+      } else if (gContextMenu.isTextSelected) {
+        var focusedWindow = document.commandDispatcher.focusedWindow;
+        var selection = focusedWindow.getSelection();
+        var text, match;
+        if (selection.rangeCount == 1 && (text = selection.toString().trim()) && text.length < 2000 && (match = base64Pattrn.exec(text)) && (match[1] in Protocols)) {
+          this._selection = selection;
+          this._decode = Protocols[match[1]].decode;
+          this._selurl = text;
+          this._menuconvt.hidden = false;
+        }
       }
       break;
     case 'command':
-      this._decode(gContextMenu.target);
+      if (evt.target == this._menudecode)
+        this._decode(gContextMenu.target);
+      else if (evt.target == this._menuconvt)
+        this._convert();
       break;
     }
   }
