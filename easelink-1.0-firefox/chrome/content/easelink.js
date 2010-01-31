9e42b26a22ffd8e1d3fa3b5600d62db7e8dc2417
ashi009 <ashi009@fa61e688-fffe-11de-87c3-653bb6514e15>
2010-01-31 18:55:32 +0000

    * Updated to 1.0.1.0 + Added First-run Detection * More flexible script * Fixed minor bugs

--- easelink-1.0-firefox/chrome/content/easelink.js
+++ easelink-1.0-firefox/chrome/content/easelink.js
@@ -1,144 +1,176 @@
 (function() {
 
-let Cc = Components.classes, Ci = Components.interfaces;
-let prefService = Cc['@mozilla.org/preferences-service;1'].getService(Ci.nsIPrefService);
-let prefBranch = prefService.getBranch('extensions.easelink.').QueryInterface(Ci.nsIPrefBranch2);
-//let log = Cc["@mozilla.org/consoleservice;1"].getService(Ci.nsIConsoleService);
+const Cc = Components.classes, Ci = Components.interfaces;
+let prefBranch = Cc['@mozilla.org/preferences-service;1'].getService(Ci.nsIPrefService)
+                                                         .getBranch('extensions.easelink.')
+                                                         .QueryInterface(Ci.nsIPrefBranch2);
 
-const urlPattern = /^(([a-zA-Z][0-9a-zA-Z+\-\.]*\:)?\/{0,2}([0-9a-zA-Z;\/:@&=\+\$\.\-_\!~\*'\(\)%]+))?([#\?]([0-9a-zA-Z;\/:@&=\+\$\.\-_\!~\*'\(\)%]*)){0,2}$/;
-const base64Pattrn = /^([a-zA-Z][0-9a-zA-Z+\-\.]*\:)\/{2}([0-9a-zA-Z\+=\/]+)([0-9a-zA-Z;\/\?:@&=\+\$\.\-_\!~\*'\(\)%#]+)?$/;
+const Version = '$VER$';
+const UrlPattern = /^(([a-zA-Z][0-9a-zA-Z+\-\.]*\:)?\/{0,2}([0-9a-zA-Z;\/:@&=\+\$\.\-_\!~\*'\(\)%]+))?([#\?]([0-9a-zA-Z;\/:@&=\+\$\.\-_\!~\*'\(\)%]*)){0,2}$/;
+const Base64Pattern = /^([a-zA-Z][0-9a-zA-Z+\-\.]*\:)\/{2}([0-9a-zA-Z\+=\/]+)([0-9a-zA-Z;\/\?:@&=\+\$\.\-_\!~\*'\(\)%#]+)?$/;
+const PartialBase64Pattern = /([a-zA-Z][0-9a-zA-Z+\-\.]*\:)\/{2}([0-9a-zA-Z\+=\/]+)([0-9a-zA-Z;\/\?:@&=\+\$\.\-_\!~\*'\(\)%#]+)?/g;
 
-var emptyFunction = function() {};
+var gL10N;
+var gAuto = false;
+var gPlain = false;
+var gEnabledAPs = {};
 
-function defaultFixer(href) {
+function defaultFixer(attname) {
   return function(node) {
-    node.setAttribute('href', node.getAttribute(href));
-    node.removeAttribute(href);
+    node.setAttribute('href', node.getAttribute(attname));
+    node.removeAttribute(attname);
     node.removeAttribute('oncontextmenu');
     node.removeAttribute('onclick');
   };
 }
 
-function defaultDecoder(prefix, suffix) {
+function defaultDecoder(prelen, suflen) {
   return function(node) {
     var url = node.getAttribute('href');
     var match;
-    if (matches = base64Pattrn.exec(url)) {
+    if (matches = Base64Pattern.exec(url)) {
       url = atob(matches[2]).toString();
-      node.setAttribute('href', url.substring(prefix, url.length - suffix));
+      node.setAttribute('href', url.substring(prelen, url.length - suflen));
     }
   };
 }
- 
-const Protocols = {
-  'thunder:': {
-    prefkey: 'thunder',
-    xpath: '[@thunderhref]',
-    fix: defaultFixer('thunderhref'),
-    decode: defaultDecoder(2, 2)
-  },
-  'qqdl:': {
-    prefkey: 'qqxuanfeng',
-    xpath: '[@qhref]',
-    fix: defaultFixer('qhref'),
-    decode: defaultDecoder(0, 0)
-  },
-  'fs2you:': {
-    xpath: "[@href and starts-with(translate(@href, 'FSYOU', 'fsyou'), 'fs2you:')]",
-    fix: emptyFunction,
-    decode: function(node) {
-      var url = node.getAttribute('href');
-      var match;
-      if (matches = base64Pattrn.exec(url)) {
-        url = atob(matches[2]).toString();
-        node.setAttribute('href', 'http://' + url.substring(0, url.lastIndexOf('|')));
-      }
-    }
-  },
-  'flashget:': {
-    xpath: "[count(@*[contains(translate(., 'FLASHGET', 'flashget'), 'flashget://')])>0]",
-    fix: function(node) {
-      var match;
-      if (node.protocol != 'flashget:') {
-        for (var i = 0; i < node.attributes.length; ++i)
-          if (match = base64Pattrn.match(node.attributes[i].nodeValue) && match[1].toLowerCase() == 'flashget:') {
-            node.setAttribute('href', 'flashget://' + match[2]);
-            node.removeAttribute(node.attributes[i].nodeName);
-            break;
-          }
-      }
-      node.removeAttribute('oncontextmenu');
-      node.removeAttribute('onclick');
-    },
-    decode: defaultDecoder(10, 10)
-  },
-  'tencent:': {
-    xpath: "[@href and starts-with(translate(@href, 'HTPWAQCOM', 'htpwaqcom'), 'http://wpa.qq.com/')]",
-    fix: function (node) {
-      var url = node.getAttribute('href');
-      var matches = urlPattern.exec(url), params;
-      if (matches && (params = matches[5]) && params.length > 0) {
-        var paramlist = {};
-        params = params.split('&');
-        for (var i = 0; i < params.length; ++i) {
-          var pair = params[i].split('=');
-          paramlist[pair[0].toLowerCase()] = pair[1];
+
+/* IProtocol definations */
+
+const IAutomaticProtocolThunder = {
+  name: 'l10n-thunder-name',
+  xpath: '[@thunderhref]',
+  fix: defaultFixer('thunderhref'),
+  decode: defaultDecoder(2, 2)
+};
+
+const IAutomaticProtocolQQXuanFeng = {
+  name: 'l10n-qqxuanfeng-name',
+  xpath: '[@qhref]',
+  fix: defaultFixer('qhref'),
+  decode: defaultDecoder(0, 0)
+};
+
+const IProtocolFlashget = {
+  xpath: "[count(@*[contains(translate(., 'FLASHGET', 'flashget'), 'flashget://')])>0]",
+  fix: function(node) {
+    var match;
+    if (node.protocol != 'flashget:') {
+      for (var i = 0; i < node.attributes.length; ++i)
+        if (match = PartialBase64Pattern.match(node.attributes[i].nodeValue) && match[1].toLowerCase() == 'flashget:') {
+          node.setAttribute('href', match[0]);
+          node.removeAttribute(node.attributes[i].nodeName);
+          break;
         }
-        url = 'tencent://Message/?menu=' + (paramlist.menu || 'no')
-            + '&exe=' + (paramlist.exe || '')
-            + '&uin=' + (paramlist.uin || '10000')
-            + '&websiteName=' + (paramlist.site || 'unknown')
-            + '&info=';
-        node.setAttribute('href', url);
-      }
-    },
-    decode: emptyFunction
+    }
+    node.removeAttribute('oncontextmenu');
+    node.removeAttribute('onclick');
   },
-  '#namipan:': {
-    xpath: "[@href and starts-with(translate(@href, 'JAVSCRIPT', 'javscript'), 'javascript:') and contains(translate(@href, 'NAMIPCO', 'namipco'), '.namipan.com')]",
-    fix: emptyFunction,
-    decode: function(node) {
-      var url = node.getAttribute('href').split("'");
-      if (url.length == 3)
-        node.setAttribute('href', decodeURIComponent(url[1]));
+  decode: defaultDecoder(10, 10)
+};
+
+const IProtocolRayFile = {
+  xpath: "[@href and starts-with(translate(@href, 'FSYOU', 'fsyou'), 'fs2you:')]",
+  decode: function(node) {
+    var url = node.getAttribute('href');
+    var match;
+    if (matches = Base64Pattern.exec(url)) {
+      url = atob(matches[2]).toString();
+      node.setAttribute('href', 'http://' + url.substring(0, url.lastIndexOf('|')));
     }
   }
 };
 
+const IProtocolQQTemporary = {
+  xpath: "[@href and starts-with(translate(@href, 'HTPWAQCOM', 'htpwaqcom'), 'http://wpa.qq.com/')]",
+  fix: function (node) {
+    var url = node.getAttribute('href');
+    var matches = UrlPattern.exec(url), params;
+    if (matches && (params = matches[5]) && params.length > 0) {
+      var paramlist = {};
+      params = params.split('&');
+      for (var i = 0; i < params.length; ++i) {
+        var pair = params[i].split('=');
+        paramlist[pair[0].toLowerCase()] = pair[1];
+      }
+      url = 'tencent://Message/?menu=' + (paramlist.menu || 'no')
+          + '&exe=' + (paramlist.exe || '')
+          + '&uin=' + (paramlist.uin || '10000')
+          + '&websiteName=' + (paramlist.site || 'unknown')
+          + '&info=';
+      node.setAttribute('href', url);
+    }
+  }
+};
+
+const IProtocolNamipan = {
+  xpath: "[@href and starts-with(translate(@href, 'JAVSCRIPT', 'javscript'), 'javascript:') and contains(translate(@href, 'NAMIPCO', 'namipco'), '.namipan.com')]",
+  decode: function(node) {
+    var url = node.getAttribute('href').split("'");
+    if (url.length == 3)
+      node.setAttribute('href', decodeURIComponent(url[1]));
+  }
+};
+
+/* IProtocol Registrators */
+
+const AutomaticProtocols = {
+  'thunder': IAutomaticProtocolThunder,
+  'qqxuanfeng': IAutomaticProtocolQQXuanFeng
+};
+
+const Protocols = {
+  'thunder:': IAutomaticProtocolThunder,
+  'qqdl:': IAutomaticProtocolQQXuanFeng,
+  'fs2you:': IProtocolRayFile,
+  'flashget:': IProtocolFlashget,
+  'tencent:': IProtocolQQTemporary,
+  '#namipan:': IProtocolNamipan
+};
+
 const StatusBar = {
   _statusbar: null,
+  _statustooltip: null,
   init: function() {
+    this._statustooltip = document.getElementById('easelink-tooltip-auto-list');
     this._statusbar = document.getElementById('easelink-status');
     this._statusbar.addEventListener('click', this, false);
   },
   handleEvent: function(evt) {
     if (evt.button == 0)
-      EaseLink.fix();
+      EaseLink.process();
   },
-  toggle: function(val) {
-    if (val)
-      this._statusbar.setAttribute('auto', true);
-    else
-      this._statusbar.removeAttribute('auto')
+  set: function() {
+    this._statusbar.setAttribute('auto', gAuto);
   }
 };
 
 const SettingMenu = {
+  _menu: null,
   _menuitems: null,
   init: function() {
-    this._menuitems = document.getElementById('easelink-setmu').getElementsByTagName('menuitem');
+    this._menu = document.getElementById('easelink-setmu');
+    for (var key in AutomaticProtocols) {
+      var menuitem = document.createElement('menuitem');
+      AutomaticProtocols[key].name = gL10N.getString(AutomaticProtocols[key].name);
+      menuitem.setAttribute('label', gL10N.getFormattedString('SettingMenu-Process-Link', [AutomaticProtocols[key].name]));
+      menuitem.setAttribute('type', 'checkbox');
+      menuitem.setAttribute('value', key);
+      this._menu.appendChild(menuitem);
+    }
+    this._menuitems = this._menu.getElementsByTagName('menuitem');
     for (var i = 0; i < this._menuitems.length; ++i) {
       if (prefBranch.getBoolPref(this._menuitems[i].getAttribute('value')))
         this._menuitems[i].setAttribute('checked', true);
       this._menuitems[i].addEventListener('command', this, true);
     }
   },
+  set: function() {
+    for (var i = 2; i < this._menuitems.length; ++i)
+      this._menuitems[i].setAttribute('disabled', !gAuto);
+  },
   handleEvent: function(evt) {
     prefBranch.setBoolPref(evt.target.getAttribute('value'), evt.target.hasAttribute('checked'));
-  },
-  toggle: function(val) {
-    for (var i = 2; i < this._menuitems.length; ++i)
-      this._menuitems[i].setAttribute('disabled', !val);
   }
 };
 
@@ -161,7 +193,7 @@ const ContextMenu = {
     var link = frag.ownerDocument.createElement('a');
     link.setAttribute('href', this._selurl);
     link.setAttribute('target', '_blank');
-    if (prefBranch.getBoolPref('plain')) this._decode(link);
+    if (gPlain) this._decode(link);
     link.appendChild(frag);
     range.insertNode(link);
   },
@@ -173,37 +205,40 @@ const ContextMenu = {
       if (gContextMenu.onLink) {
         var link = gContextMenu.link;
         var doc = link.ownerDocument;
-        var plain = prefBranch.getBoolPref('plain') && gContextMenu.onSaveableLink;
-        var fixed = false, found = false;
+        var orghref = link.getAttribute('href');
+        var plain = gPlain && gContextMenu.onSaveableLink;
+        var fixed = false;
+        var found = false;
         for each (var protocol in Protocols)
           if (doc.evaluate('.' + protocol.xpath, link, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue) {
-            protocol.fix(link);
+            if ('fix' in protocol) protocol.fix(link);
             this._decode = protocol.decode;
             fixed = true;
             break;
           }
-        if (!fixed && (link.protocol in Protocols)) {
+        if (!fixed && link.protocol in Protocols) {
           this._decode = Protocols[link.protocol].decode;
           found = true;
         }
         if (fixed || found) {
-          if (plain)
+          if (!plain)
+            this._menudecode.hidden = !this._decode;
+          else if (this._decode)
             this._decode(link);
-          else
-            this._menudecode.hidden = this._decode == emptyFunction;
-        }
-        if (fixed || found && plain) {
-          gContextMenu.linkURL = gContextMenu.getLinkURL();
-          gContextMenu.linkURI = gContextMenu.getLinkURI();
-          gContextMenu.linkProtocol = gContextMenu.getLinkProtocol();
+          if (link.getAttribute('href') != orghref) {
+            gContextMenu.linkURL = gContextMenu.getLinkURL();
+            gContextMenu.linkURI = gContextMenu.getLinkURI();
+            gContextMenu.linkProtocol = gContextMenu.getLinkProtocol();
+          }
         }
       } else if (gContextMenu.isTextSelected) {
         var focusedWindow = document.commandDispatcher.focusedWindow;
         var selection = focusedWindow.getSelection();
-        var text, match;
-        if (selection.rangeCount == 1 && (text = selection.toString().trim()) && text.length < 2000 && (match = base64Pattrn.exec(text)) && (match[1] in Protocols)) {
+        var text, match, protocol;
+        if (selection.rangeCount == 1 && (text = selection.toString().trim()) && text.length < 2000
+            && (match = UrlPattern.exec(text)) && (protocol = match[2].toLowerCase()) in Protocols) {
           this._selection = selection;
-          this._decode = Protocols[match[1]].decode;
+          this._decode = Protocols[protocol].decode;
           this._selurl = text;
           this._menuconvt.hidden = false;
         }
@@ -220,53 +255,106 @@ const ContextMenu = {
 };
 
 const EaseLink = {
-  _auto: false,
   init: function() {
+    l10n = document.getElementById('easelink-strings');
     StatusBar.init();
     SettingMenu.init();
     ContextMenu.init();
-    prefBranch.addObserver('auto', this, false);
-    this._toggle();
+    this._checkFirstRun();
+    prefBranch.addObserver('', this, false);
+    this._refreshEnabledList(true);
+    this._refreshPlain();
+    this._refreshAuto();
   },
-  _toggle: function(){
-    var val = prefBranch.getBoolPref('auto');
-    if (this._auto ^ val) {
-      if (val)
+  _checkFirstRun: function() {
+    if (prefBranch.getCharPref('ver') != Version) {
+      prefBranch.setCharPref('ver', Version);
+      window.setTimeout(function() {
+        gBrowser.selectedTab = gBrowser.addTab(gL10N.getString('First_Run_URL'));
+      }, 1500);
+    }
+  },
+  _refreshAuto: function() {
+    var exp = prefBranch.getBoolPref('auto');
+    if (gAuto ^ exp) {
+      if (exp)
         gBrowser.addEventListener('DOMContentLoaded', this, true);
       else
         gBrowser.removeEventListener('DOMContentLoaded', this, true);
-      this._auto = val;
-      StatusBar.toggle(val);
-      SettingMenu.toggle(val);
+      gAuto = exp;
+      StatusBar.set();
+      SettingMenu.set();
+    }
+  },
+  _refreshPlain: function() {
+    var exp = prefBranch.getBoolPref('plain');
+    if (gPlain ^ exp)
+      gPlain = exp;
+  },
+  _refreshEnabledList: function(init, key) {
+    if (init) {
+      for (var key in AutomaticProtocols)
+        if (prefBranch.getBoolPref(key))
+          gEnabledAPs[key] = AutomaticProtocols[key];
+    } else {
+      var exp = prefBranch.getBoolPref(key);
+      if ((key in gEnabledAPs) ^ exp) {
+        if (exp)
+          gEnabledAPs[key] = AutomaticProtocols[key];
+        else
+          delete gEnabledAPs[key];
+        StatusBar.refresh();
+      }
     }
   },
   observe: function(subject, topic, data) {
-    if (topic != 'nsPref:changed')
-      return;
-    this._toggle();
+    if (topic != "nsPref:changed")
+			return;
+    switch (data) {
+      case 'auto':
+        this._refreshAuto();
+        break;
+      case 'plain':
+        this._refreshPlain();
+        break;
+      case 'ver':
+        break;
+      default:
+        this._refreshEnabledList(false, data);
+        break;
+    }
   },
   handleEvent: function(evt) {
-    this.fix(true, evt.target);
-  },
-  fix: function(partial, target) {
-    var doc = target || gBrowser.contentDocument;
+    var doc = evt.target;
     var iterator, node, nodes = [];
-    var plain = prefBranch.getBoolPref('plain');
-    if (doc.documentElement) for each (var protocol in Protocols) {
-      if (!protocol.prefkey || partial && !prefBranch.getBoolPref(protocol.prefkey)) continue;
+    if (doc.documentElement) for each (var protocol in gEnabledAPs) {
       iterator = doc.evaluate('.//a' + protocol.xpath, doc.documentElement, null, XPathResult.ANY_TYPE, null);
-      while (!iterator.invalidIteratorState && (node = iterator.iterateNext()) && (!partial || nodes.length <= 10))
+      while (!iterator.invalidIteratorState && (node = iterator.iterateNext()) && nodes.length <= 10)
         nodes.push(node);
       while ((node = nodes.pop()) || nodes.length) {
         protocol.fix(node);
-        if (plain) protocol.decode(node);
+        if (gPlain) protocol.decode(node);
+      }
+    }
+  },
+  process: function(doc) {
+    doc = doc || gBrowser.contentDocument;
+    var iterator, node, nodes = [];
+    if (doc.documentElement) for each (var protocol in Protocols) {
+      iterator = doc.evaluate('.//a' + protocol.xpath, doc.documentElement, null, XPathResult.ANY_TYPE, null);
+      while (!iterator.invalidIteratorState && (node = iterator.iterateNext()) && nodes.length <= 50)
+        nodes.push(node);
+      while ((node = nodes.pop()) || nodes.length) {
+        if ('fix' in protocol) protocol.fix(node);
+        if (gPlain && 'decode' in protocol) protocol.decode(node);
       }
     }
   }
 };
 
 window.EaseLink = {
-  fix: EaseLink.fix
+  fix: EaseLink.process,
+  processAll: EaseLink.process
 };
 
 window.addEventListener('load', function() {EaseLink.init()}, false);
