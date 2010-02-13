a6bbd4dfd6a848ee5de849f0ea3da1f0582092d4
ashi009 <ashi009@fa61e688-fffe-11de-87c3-653bb6514e15>
2010-02-13 01:32:10 +0000

    * Fixed Flashget Link conversion issue * Fixed Minor bugs

--- trunk/easelink-1.0-firefox/chrome/content/easelink.js
+++ trunk/easelink-1.0-firefox/chrome/content/easelink.js
@@ -55,17 +55,16 @@ const IProtocolFlashget = {
   xpath: "[count(@*[contains(translate(., 'FLASHGET', 'flashget'), 'flashget://')])>0]",
   fix: function(node) {
     var match;
-    if (node.protocol == 'flashget:') {
-      var url = node.getAttribute('href');
-      node.setAttribute('href', url.substring(0, url.lastIndexOf('&')));
-    } else {
+    if (node.protocol != 'flashget:')
       for (var i = 0; i < node.attributes.length; ++i)
         if (match = PartialBase64Pattern.match(node.attributes[i].nodeValue) && match[1].toLowerCase() == 'flashget:') {
           node.setAttribute('href', match[0]);
           node.removeAttribute(node.attributes[i].nodeName);
           break;
         }
-    }
+    var url = node.getAttribute('href');
+    var pos = url.lastIndexOf('&');
+    if (pos != -1) node.setAttribute('href', url.substring(0, pos));
     node.removeAttribute('oncontextmenu');
     node.removeAttribute('onclick');
   },
@@ -180,6 +179,7 @@ const SettingMenu = {
 const ContextMenu = {
   _menudecode: null,
   _menuconvt: null,
+  _fix: null,
   _decode: null,
   _selection : null,
   _selurl : "",
@@ -196,7 +196,8 @@ const ContextMenu = {
     var link = frag.ownerDocument.createElement('a');
     link.setAttribute('href', this._selurl);
     link.setAttribute('target', '_blank');
-    if (gPlain) this._decode(link);
+    if (this._fix) this._fix(link);
+    if (gPlain && this._decode) this._decode(link);
     link.appendChild(frag);
     range.insertNode(link);
   },
@@ -238,9 +239,10 @@ const ContextMenu = {
         var focusedWindow = document.commandDispatcher.focusedWindow;
         var selection = focusedWindow.getSelection();
         var text, match, protocol;
-        if (selection.rangeCount == 1 && (text = selection.toString().trim()) && text.length < 2000
+        if (selection.rangeCount == 1 && (text = selection.toString().trim()) && text.length < 1000
             && (match = UrlPattern.exec(text)) && (protocol = match[2].toLowerCase()) in Protocols) {
           this._selection = selection;
+          this._fix = Protocols[protocol].fix;
           this._decode = Protocols[protocol].decode;
           this._selurl = text;
           this._menuconvt.hidden = false;
